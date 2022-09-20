d80ebcef756eb07e72a58ad5be67e497b9ccf573
LeonardoDaRocha745 <leonardodaredevil@gmail.com>
2022-09-20 18:21:06 -0300

    Configurando domains

---
+++ src/main/java/com/leonardo/gamestore/domain/Jogo.java
@@ -0,0 +1,10 @@
+package com.leonardo.gamestore.domain;
+
+public class Jogo {
+	private Integer id;
+	private String titulo;
+	private String plataforma;
+	private String texto;
+	
+	private Categoria categoria;
+}
